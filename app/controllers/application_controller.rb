80f45e0f572ab379135adf0a99a2b61a840ba107
eugenielm <eugenie.le.moulec@gmail.com>
2017-07-18 20:45:59 -0700

    WIP: limiting the scope of all object-related changes to admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # before_action :require_login, :only => [:destroy, :create, :edit, :new, :update]
 
   def hello
     render html: "hello, world!"
   end
+  #
+  # private
+  #
+  #   def require_login
+      # this filter method is inherited by all the controllers of the app for
+      # all the above-mentionned actions
+    #   unless logged_in && session[:user.name] == "adminEG"?
+    #     flash[:error] = "You must be logged in as the admin to modify data."
+    #     redirect_to new_login_url
+    #   end
+    # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
   resources :categories do
     resources :pictures, :except => [:destroy, :create, :edit, :new, :update]
   end
-  resources :pictures, :only => [:destroy, :create, :edit, :new, :update]
-  root 'welcome#index'
+
+  scope module: 'admin' do
+    resources :pictures, :only => [:destroy, :create, :edit, :new, :update]
+  end
+
 end
