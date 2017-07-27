34eb0ac55a9436f202418d7c7d0c72abd2fea2b0
eugenielm <eugenie.le.moulec@gmail.com>
2017-07-26 21:21:30 -0700

    Use SSL and the Puma webserver in production (Heroku)

--- config/puma.rb
+++ config/puma.rb
@@ -23,6 +23,12 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+
+# https://www.railstutorial.org/book/sign_up#code-production_webserver_config
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
@@ -30,7 +36,21 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-# preload_app!
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+# (end) https://www.railstutorial.org/book/sign_up#code-production_webserver_config
+
+
 
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
