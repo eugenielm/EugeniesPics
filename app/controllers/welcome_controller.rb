64df5010ada8dc34dd961e54ce57669d37655108
eugenielm <eugenie.le.moulec@gmail.com>
2017-07-27 17:14:16 -0700

    WIP links displayed depending on the user's authentification status

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   def index
     # render html: "Welcome to the index page!"
   end
+
+  def about
+    # render html: "Welcome to the index page!"
+  end
 end
