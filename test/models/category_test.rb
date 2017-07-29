859dabdf588c599ff6d4e0cbd6b616ff9343df17
eugenielm <eugenie.le.moulec@gmail.com>
2017-07-28 19:22:17 -0700

    Write category model tests and fixtures

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "category should be valid" do
+    assert categories(:one).valid?
+  end
+
+  test "name should be present" do
+    assert_not categories(:two).valid?
+  end
+
+  test "name shouldn't be at least 2 characters long" do
+    assert_not categories(:three).valid?
+  end
+
+  test "name shouldn't be more than 30 characters long" do
+    assert_not categories(:four).valid?
+  end
+
 end
