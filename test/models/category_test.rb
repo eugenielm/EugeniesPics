d73f8eb06af937e0b3eeff47903f79845b6b06e6
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-01 09:55:13 -0700

    Update category fixtures and category model tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
+  setup do
+    @cat2 = Category.new(name: "")
+    @cat3 = Category.new(name: "c")
+    @cat4 = Category.new(name: "a" * 31)
+  end
 
   test "category should be valid" do
     assert categories(:one).valid?
   end
 
   test "name should be present" do
-    assert_not categories(:two).valid?
+    assert_not @cat2.valid?
   end
 
   test "name shouldn't be at least 2 characters long" do
-    assert_not categories(:three).valid?
+    assert_not @cat3.valid?
   end
 
   test "name shouldn't be more than 30 characters long" do
-    assert_not categories(:four).valid?
+    assert_not @cat4.valid?
   end
 
 end
