3c63d4159c25b7a4f209d6ede000237aacd80b0e
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-01 19:28:30 -0700

    Update pictures controller test and fix pictures controller accordingly

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -22,7 +22,8 @@ class PicturesController < ApplicationController
 
     respond_to do |format|
       if @picture.save # returns false if the params are invalid
-        format.html { redirect_to categories_url, notice: 'Picture was successfully created.' }
+        flash[:info] = 'Picture was successfully created.'
+        format.html { redirect_to category_pictures_url(@category) }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
@@ -34,9 +35,9 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params) # returns false if the params are invalid
+        flash[:info] = 'Picture was successfully updated.'
         format.html { redirect_to :controller => 'pictures', :action => 'show',
-                                  :category_id => @picture.category.id , :id => @picture.id,
-                                  notice: 'Picture was successfully updated.' }
+                                  :category_id => @picture.category.id , :id => @picture.id }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
@@ -48,9 +49,9 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
+      flash[:info] = 'Picture was successfully destroyed.'
       format.html { redirect_to :controller => 'pictures', :action => 'index',
-                                :category_id => @category.id,
-                                notice: 'Picture was successfully destroyed.' }
+                                :category_id => @category.id }
       format.json { head :no_content }
     end
   end
@@ -66,7 +67,7 @@ class PicturesController < ApplicationController
     def require_category
       @category = Category.find(params[:category_id]) rescue @category = nil
       if @category.nil?
-        flash[:danger] = "The required category doesn't exist"
+        flash[:danger] = "The required category doesn't exist."
         redirect_to categories_path
       end
     end
@@ -74,16 +75,20 @@ class PicturesController < ApplicationController
     def require_picture
       @picture = Picture.find(params[:id]) rescue @picture = nil
       if @picture.nil?
-        flash[:danger] = "The required picture doesn't exist"
+        flash[:danger] = "The required picture doesn't exist."
         redirect_to category_pictures_path(@category)
       end
     end
 
     def admin_power
-      if !is_superadmin?
+      if !logged_in?
         session[:prev_url] = request.fullpath
-        flash[:danger] = "You need to be logged in as an admin for this action"
+        flash[:info] = "You need to be logged in for this action."
         redirect_to login_path
+      elsif !is_superadmin?
+        flash[:danger] = "Unauthorized action."
+        redirect_to root_path
       end
     end
+
 end
--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -1,7 +1,130 @@
 require 'test_helper'
 
 class PicturesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = categories(:one)
+    @picture = pictures(:Pict1)
+    @user_non_admin = users(:one)
+    @user_admin = users(:two)
+  end
+
+  test "should get pictures index" do
+    get category_pictures_url(@category)
+    assert_response :success
+  end
+
+  test "should get picture detail page" do
+    get category_picture_url(@category, @picture)
+    assert_response :success
+  end
+
+  # new picture
+  test "unauthenticated user should be redirected to login url when new picture" do
+    get new_category_picture_url(@category)
+    assert_redirected_to login_url
+    follow_redirect!
+    assert_equal 'You need to be logged in for this action.', flash[:info]
+  end
+
+  test "authenticated non-admin user should be redirected to root_url when new picture" do
+    post login_url, params: { session: { email: @user_non_admin.email, password: "nonadminpassword" }}
+    get new_category_picture_url(@category)
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_equal 'Unauthorized action.', flash[:danger]
+  end
+
+  test "authenticated admin user should get new picture" do
+    post login_url, params: { session: { email: @user_admin.email, password: "adminpassword" }}
+    get new_category_picture_url(@category)
+    assert_response :success
+  end
+
+  # create
+  test "authenticated admin user should create picture" do
+    post login_url, params: { session: { email: @user_admin.email, password: "adminpassword" }}
+    assert_difference('Picture.count') do
+      post category_pictures_url(@category), params: { picture: { title: 'new picture', author: 'EG', description: 'blah', category_id: @category.id } }
+    end
+    assert_redirected_to category_pictures_url(@category)
+    follow_redirect!
+    assert_equal 'Picture was successfully created.', flash[:info]
+  end
+
+  # edit
+  test "unauthenticated user should be redirected to login when get edit picture" do
+    get edit_category_picture_url(@category, @picture)
+    assert_redirected_to login_url
+    follow_redirect!
+    assert_equal 'You need to be logged in for this action.', flash[:info]
+  end
+
+  test "authenticated non-admin user should be redirected to root when get edit picture" do
+    post login_url, params: { session: { email: @user_non_admin.email, password: "nonadminpassword" }}
+    get edit_category_picture_url(@category, @picture)
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_equal 'Unauthorized action.', flash[:danger]
+  end
+
+  test "authenticated admin user should get edit picture" do
+    post login_url, params: { session: { email: @user_admin.email, password: "adminpassword" }}
+    get edit_category_picture_url(@category, @picture)
+    assert_response :success
+  end
+
+  # update
+  test "unauthenticated user should be redirected to login when patch update picture" do
+    patch category_picture_url(@category, @picture), params: { picture: { title: 'new title', author: 'EG', description: 'blah', category_id: @category.id } }
+    assert_redirected_to login_url
+    follow_redirect!
+    assert_equal 'You need to be logged in for this action.', flash[:info]
+  end
+
+  test "authenticated non-admin user should be redirected to root when patch update picture" do
+    post login_url, params: { session: { email: @user_non_admin.email, password: "nonadminpassword" }}
+    patch category_picture_url(@category, @picture), params: { picture: { title: 'new title', author: 'EG', description: 'blah', category_id: @category.id } }
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_equal 'Unauthorized action.', flash[:danger]
+  end
+
+  test "authenticated admin user should be able to update picture" do
+    post login_url, params: { session: { email: @user_admin.email, password: "adminpassword" }}
+    patch category_picture_url(@category, @picture), params: { picture: { title: 'new title', author: 'EG', description: 'blah', category_id: @category.id } }
+    assert_redirected_to category_picture_url(@category, @picture)
+    follow_redirect!
+    assert_equal 'Picture was successfully updated.', flash[:info]
+  end
+
+  # destroy
+  test "unauthenticated user should be redirected to login when destroy picture" do
+    assert_difference('Picture.count', 0) do
+      delete category_picture_url(@category, @picture)
+    end
+    assert_redirected_to login_url
+    follow_redirect!
+    assert_equal 'You need to be logged in for this action.', flash[:info]
+  end
+
+  test "authenticated non-admin user should be redirected to login when destroy picture" do
+    post login_url, params: { session: { email: @user_non_admin.email, password: "nonadminpassword" }}
+    assert_difference('Picture.count', 0) do
+      delete category_picture_url(@category, @picture)
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_equal 'Unauthorized action.', flash[:danger]
+  end
+
+  test "authenticated admin user should destroy picture" do
+    post login_url, params: { session: { email: @user_admin.email, password: "adminpassword" }}
+    assert_difference('Picture.count', -1) do
+      delete category_picture_url(@category, @picture)
+    end
+    assert_redirected_to category_pictures_url(@category)
+    follow_redirect!
+    assert_equal 'Picture was successfully destroyed.', flash[:info]
+  end
+
 end
