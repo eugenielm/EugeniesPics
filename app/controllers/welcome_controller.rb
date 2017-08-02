948acf4cbe56df571fcf09085b421c0dabdc1fc2
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-01 19:31:00 -0700

    Update welcome controller test and fix welcome controller accordingly

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    # render html: "Welcome to the index page!"
   end
 
   def about
-    # render html: "Welcome to the index page!"
   end
 end
