f7795be5bddab2c327210bb6927491610cfdb272
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-04 11:54:14 -0700

    setting config.serve_static_files to false for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # https://github.com/thoughtbot/paperclip_demo/blob/master/config/environments/production.rb
-  # config.serve_static_files = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
