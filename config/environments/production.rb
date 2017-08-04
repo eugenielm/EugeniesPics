c65f96806ae60eb306ba09728c3a0792f95cd01a
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-04 15:00:13 -0700

    Change config.paperclip_defaults to fix AWS S3 assets url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,12 +97,15 @@ Rails.application.configure do
   # AWS S3 storage for pictures :picfile attribute
   config.paperclip_defaults = {
   storage: :s3,
-  s3_protocol: 'http',
+  url: ':s3_domain_url',
+  # s3_protocol: 'http',
+  path: ':class/:attachment/:id_partition/:style/:filename',
+  s3_region: ENV.fetch('AWS_REGION'), # us-west-1
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    :s3_host_name => 's3.amazonaws.com'# or: 's3-us-west-1.amazonaws.com'?
   }
 }
 end
