f3993d44a78590a366d171da8fb3ce36f4e98d52
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-16 16:32:57 -0700

    Update public/packs-test/application.js

--- public/packs-test/application.js
+++ public/packs-test/application.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "/packs-test/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 185);
+/******/ 	return __webpack_require__(__webpack_require__.s = 198);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 185:
+/***/ 198:
 /***/ (function(module, exports) {
 
 /* eslint no-console:0 */
