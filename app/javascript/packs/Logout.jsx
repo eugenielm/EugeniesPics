58f72e9bb2dc0a0a96ec757174645b6652579a64
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-24 17:35:03 -0700

    Clean Logout.jsx

--- app/javascript/packs/Logout.jsx
+++ app/javascript/packs/Logout.jsx
@@ -1,9 +1,5 @@
-// Run this example by adding <%= javascript_pack_tag 'Logout' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
 const Logout = props => {
   if (props.user) {
