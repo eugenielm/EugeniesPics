2df31bf6143735535b57f74eb5eaef3ec21430a2
eugenielm <eugenie.le.moulec@gmail.com>
2017-08-24 17:18:54 -0700

    Create ErrorComponent to handle error messages when invalid input in forms

---
+++ app/javascript/packs/ErrorsComponent.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'babel-polyfill' // needed for the generator IndexMaker()
+
+function* IndexMaker(){
+    let i = 0;
+    while (true) {
+        yield i++;
+    }
+}
+
+const gen = IndexMaker();
+
+const ErrorListElement = props => {
+    return (<li>{props.err}</li>)
+};
+
+const ErrorsComponent = props => {
+    const obj = props.model;
+    if (props.errors) {
+        return (
+            <div id="error_explanation">
+                <h2>The following error(s) prohibited this {obj} from being saved:</h2>
+                <ul>
+                    {props.errors.map(e => <ErrorListElement err={e} key={gen.next().value} />)}
+                </ul>
+            </div>
+        );
+    }
+    return null;
+};
+
+export default ErrorsComponent;
