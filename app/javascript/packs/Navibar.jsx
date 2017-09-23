7c47b83cde83d1e29d9e86c15d9effee17958eb1
eugenielm <eugenie.le.moulec@gmail.com>
2017-09-22 19:16:04 -0700

    WIP page header without navbar nor dropdown

--- app/javascript/packs/Navibar.jsx
+++ app/javascript/packs/Navibar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Button, ButtonGroup, ButtonToolbar, SplitButton, MenuItem, Navbar, Nav, NavItem, NavDropdown, Jumbotron, Grid, Row, Col, Well, PageHeader } from 'react-bootstrap';
 import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
 
 
 const CategoriesDropdownElement = (props) => {
-  return (<LinkContainer to={'/categories/' + props.cat_id + '/pictures'}>
-            <MenuItem bsSize="small" active={(props.path == '/categories/' + props.cat_id + '/pictures') ? true : false}>
+  return (<IndexLinkContainer to={'/categories/' + props.cat_id + '/pictures'}>
+            <Button active={(props.path == '/categories/' + props.cat_id + '/pictures') ? true : false}>
               {props.cat_name}
-            </MenuItem>
-          </LinkContainer>);
+            </Button>
+          </IndexLinkContainer>);
 }
 
 
@@ -20,7 +20,6 @@ class Navibar extends React.Component {
     super(props);
     this.state = {categories: [], path: this.props.location.pathname, dropdownOpen: false, navbarExpanded: false};
     this.handleDropdown = this.handleDropdown.bind(this);
-    this.handleNavbarExpanded = this.handleNavbarExpanded.bind(this);
   }
 
   componentDidMount() {
@@ -41,50 +40,30 @@ class Navibar extends React.Component {
     this.setState({ dropdownOpen: !this.state.dropdownOpen })
   }
 
-  handleNavbarExpanded() {
-    this.setState({ navbarExpanded: !this.state.navbarExpanded })
-  }
 
   render () {
-
-    const dropdown = <NavDropdown title="Going places" id="categories-nav-dropdown" noCaret 
-                      onToggle={this.handleDropdown} open={this.state.dropdownOpen}
-                      active={(this.state.path != '/' && this.state.path != '/about') ? true : false} >
-                      { this.state.path == '/categories' ?
-                        <MenuItem href="/categories" active={true}>Overview</MenuItem>
-                        :
-                        <IndexLinkContainer to='/categories'>
-                          <MenuItem active={false}>Overview</MenuItem>
-                        </IndexLinkContainer>
-                      }
-                        <MenuItem divider />
-                        { this.state.categories.map(c => <CategoriesDropdownElement key={c.id} cat_id={c.id} cat_name={c.name} path={this.state.path} />) }
-                    </NavDropdown>
 
     return (
       
       <div id="page-header">
-        <p>EUGENIE'S PICS</p>
-        <p>Photographs by Eugénie Le Moulec</p>
+        <Link to='/' id="home-link">
+          <p>EUGENIE'S PICS</p>
+          <p>Photographs by Eugénie Le Moulec</p>
+        </Link>
 
         <br/>
                     
-          <Navbar expanded={this.state.navbarExpanded ? true : false} onToggle={this.handleNavbarExpanded}>
-            <Navbar.Header>
-              <IndexLinkContainer to='/'>
-                <Navbar.Brand>LOGO</Navbar.Brand>
-              </IndexLinkContainer>
-              <Navbar.Toggle/>
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav>
-                  { dropdown }
-                  <IndexLinkContainer to='/about'><NavItem active={this.state.path == '/about' ? true : false}>About</NavItem></IndexLinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+        <div className="d-flex justify-content-around">
+            <IndexLinkContainer to='/about'><Button>About</Button></IndexLinkContainer>
+            { this.state.path == '/categories' ?
+                <Button href="/categories">Going places</Button>
+                  :
+                <IndexLinkContainer to='/categories'><Button>Going places</Button></IndexLinkContainer>
+            }
+            { this.state.categories.map(c => <CategoriesDropdownElement key={c.id} cat_id={c.id} cat_name={c.name} path={this.state.path} />) }
+        </div>
                   
-        { this.props.user ? <Button id="id-button" bsStyle="default" bsSize="small" data-method="delete" href="/logout">Log out</Button> : null }
+        { this.props.user ? <Button id="id-button" bsStyle="info" bsSize="small" data-method="delete" href="/logout">Log out</Button> : null }
       </div>
 
     )
