42baa0b6f8c7d0c634e543162bdcd8b7f01aaf54
eugenielm <eugenie.le.moulec@gmail.com>
2017-10-03 17:25:58 -0700

    Update HomePage component title and increase pictures turnover speed

--- app/javascript/packs/HomePage.jsx
+++ app/javascript/packs/HomePage.jsx
@@ -8,7 +8,7 @@ class HomePage extends React.Component {
         this.setState({ picsSelection: [], picIndex: 0 });
         this.changePic = this.changePic.bind(this);
         this.changeSlides = this.changeSlides.bind(this);
-        this.timer = setInterval(this.changePic, 5000);
+        this.timer = setInterval(this.changePic, 3000);
     }
 
     changePic() {
@@ -34,7 +34,7 @@ class HomePage extends React.Component {
                 this.setState({ picIndex: this.state.picIndex - 1 });
             }
         }
-        this.timer = setInterval(this.changePic, 5000);
+        this.timer = setInterval(this.changePic, 3000);
     }
 
     componentDidMount() {
@@ -54,13 +54,13 @@ class HomePage extends React.Component {
     render() {
         return (
             <div id="home-page">
-                <div id="home-page-title">Welcome to Eugénie's photography gallery!</div>
+                <div className="page-title">Welcome!</div>
                 
                 {this.state.picsSelection.length > 0 ?
                     (<div id="pic-element">
                         <Link to={"/categories/" + (this.state.picsSelection[this.state.picIndex].selectionPicCatId) + "/pictures"}>
                             <img src={this.state.picsSelection[this.state.picIndex].selectionPicUrl} />
-                            <div id="category_name">{this.state.picsSelection[this.state.picIndex].selectionPicCatName.toUpperCase()}</div>
+                            <div id="category-name">{this.state.picsSelection[this.state.picIndex].selectionPicCatName.toUpperCase()}</div>
                         </Link>
                         <div className="prev" onClick={() => this.changeSlides(-1)}><span className="glyphicon glyphicon-chevron-left"></span></div>
                         <div className="next" onClick={() => this.changeSlides(1)}><span className="glyphicon glyphicon-chevron-right"></span></div>
