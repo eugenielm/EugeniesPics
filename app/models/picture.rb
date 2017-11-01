21942d74fe9479eb449589f74815f8fed4d73c52
eugenielm <eugenie.le.moulec@gmail.com>
2017-11-01 16:52:06 -0700

    Increases allowed pictures weight

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -14,10 +14,10 @@ class Picture < ApplicationRecord
   validates :author, presence: true, length: { minimum: 2, maximum: 30 }
   
   # http://www.rubydoc.info/gems/paperclip/Paperclip/ClassMethods
-  has_attached_file :picfile, styles: { medium: "1000x1000>", small: "300x300>" },
+  has_attached_file :picfile, styles: { medium: "1500x1500>", small: "500x500>" },
                     default_style: 'small'
   validates_attachment :picfile, presence: true,
-                       size: { in: 0..1500.kilobytes }
+                       size: { in: 0..4000.kilobytes }
   validates_attachment_file_name :picfile, matches: [/png\z/i, /jpe?g\z/i]
   validates :picfile_file_name, uniqueness: { case_sensitive: false }
 
