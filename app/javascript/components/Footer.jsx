225504511f85f704d23e0032794ff48f96db50cf
eugenielm <eugenie.le.moulec@gmail.com>
2017-11-24 22:32:52 -0800

    Replace 'created' by 'home-made' in footer

--- app/javascript/components/Footer.jsx
+++ app/javascript/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = props => (
     <div id="footer">
-        <p>(c)2017 Eugénie Le Moulec - created with passion <em>^_^</em></p>
+        <p>(c)2017 Eugénie Le Moulec - home-made with passion <em>^_^</em></p>
     </div>
 );
 
